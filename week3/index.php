fcef8748be15e027f624b51a3d407eefdca9c359
MichielPronk <michiel_pronk@upcmail.nl>
2020-11-30 15:32:37 +0100

    Exercise 5&6

--- week3/index.php
+++ week3/index.php
@@ -18,7 +18,21 @@ $db = connect_db('localhost', 'ddwt20_week3', 'ddwt20', 'ddwt20');
 /* Create Router instance */
 $router = new \Bramus\Router\Router();
 
-// Add routes here
 
+// Add routes here
+$cred = set_cred('ddwt20', 'ddwt20');
+
+$router->get('/api/series/', function() use ($cred){
+    if (!check_cred($cred)){
+        echo 'Authentication required.';
+        http_response_code(401);
+        die();
+    }
+    echo "Succesfully authenticated";
+});
+
+$router->set404(function() {
+    header('HTTP/1.1 404 Not Found');
+    echo 'This page was not found';});
 /* Run the router */
 $router->run();
