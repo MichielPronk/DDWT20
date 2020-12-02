c701e2ab1bf00ee4de90060475e3425084a98c04
MichielPronk <michiel_pronk@upcmail.nl>
2020-12-02 15:59:51 +0100

    Exercises 10 to 12 added

--- week3/index.php
+++ week3/index.php
@@ -6,7 +6,6 @@
  * Time: 15:25
  */
 
-
 /* Require composer autoloader */
 require __DIR__ . '/vendor/autoload.php';
 
@@ -24,35 +23,54 @@ header(http_content_type('application/json'));
 
 $cred = set_cred('ddwt20', 'ddwt20');
 
-$router->get('/api/series/', function() use ($cred, $db, $router){
+$router->before('GET|POST|PUT|DELETE', '/api/.*', function() use($cred){
     if (!check_cred($cred)){
         echo 'Authentication required.';
         http_response_code(401);
         die();
     }
-    echo "Succesfully authenticated";
 });
 
-$router->get('/series', function() use($db) {
-    $series = get_series($db);
-    $series = json_encode($series);
-    echo $series;
-});
+$router->mount('/api', function() use ($router, $db) {
 
-$router->get('/series/(\d+)', function($id) use($db) {
-    $info = get_serieinfo($db, $id);
-    $info = json_encode($info);
-    echo $info;
-});
+    $router->get('/series', function() use($db) {
+        $series = get_series($db);
+        $series = json_encode($series);
+        echo $series;
+    });
 
-$router->get('/delete/(\d+)', function($id) use($db) {
-    $feedback = remove_serie($db, $id);
-    $feedback = json_encode($feedback['message']);
-    echo $feedback;
+    $router->get('/series/(\d+)', function($id) use($db) {
+        $info = get_serieinfo($db, $id);
+        $info = json_encode($info);
+        echo $info;
+    });
+
+    $router->get('/delete/(\d+)', function($id) use($db) {
+        $feedback = remove_serie($db, $id);
+        $feedback = json_encode($feedback['message']);
+        echo $feedback;
+    });
+
+    $router->post('/series', function () use ($db) {
+        $feedback = add_serie($db, $_POST);
+        $feedback = json_encode($feedback['message']);
+        echo $feedback;
+    });
+
+    $router->put('/series/(\d+)', function ($id) use($db){
+        $_PUT = array();
+        parse_str(file_get_contents('php://input'), $_PUT);
+        $serie_info = $_PUT + ["serie_id" => $id];
+        $feedback = update_serie($db, $serie_info);
+        $feedback = json_encode($feedback['message']);
+        echo $feedback;
+    });
+
 });
 
 $router->set404(function() {
     header('HTTP/1.1 404 Not Found');
-    echo 'This page was not found';});
-/* Run the router */
+    echo 'This page was not found';
+});
+
 $router->run();
